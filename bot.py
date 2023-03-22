7ceaa2074d969083d7004f14645f88418a00477d
Xenia Zagvozdina <xenia.zagvozdina@yandex.ru>
2023-03-23 01:21:55 +0300

    cointergation + linear regression

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
 from binance.client import Client
-from sklearn.decomposition import PCA
-from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
 from constants import BINANCE_API_KEY, BINANCE_API_SECRET
+import time
 
 # Параметры подключения к Binance API
 api_key = BINANCE_API_KEY
@@ -26,38 +26,59 @@ def get_klines(symbol, interval, n):
 # Последние 120 свечей фьючерса ETHUSDT и BTCUSDT с интервалом 1 минута
 data_ETHUSDT = get_klines('ETHUSDT', Client.KLINE_INTERVAL_1MINUTE, 120)
 data_BTCUSDT = get_klines('BTCUSDT', Client.KLINE_INTERVAL_1MINUTE, 120)
-# print(data_ETHUSDT)
-# print(data_BTCUSDT)
 
 # Только Close price
-eth_prices = data_ETHUSDT['Close'].values
-btc_prices = data_BTCUSDT['Close'].values
-prices = np.column_stack((eth_prices, btc_prices))
+y = data_ETHUSDT['Close'].values
+x = data_BTCUSDT['Close'].values #+ data_ETHUSDT[['Open', 'High', 'Low', 'Close', 'Volume']]
 
-# Стандартизация данных
-prices_std = (prices - prices.mean(axis=0)) / prices.std(axis=0)
+# Тест на коинтеграцию, уровень значимости 0.05
+coint = sm.tsa.stattools.coint(y, x)
+if coint[1] < 0.05:
+    print("The variables are cointegrated.")
+else:
+    print("The variables are not cointegrated.")
 
-# Метод главных компонент
-pca = PCA(n_components=2)
-principal_components = pca.fit_transform(prices_std)
+# Новые параметры
+x = x #+ data_ETHUSDT[['Open', 'High', 'Low', 'Close', 'Volume']]
 
-# Исключение движений, связанных с BTCUSDT
-eth_prices_excl_btc = principal_components[:, 0]
+# Регрессия
+model = sm.OLS(y, sm.add_constant(x))
+results = model.fit()
+# print(results.summary())
 
-# print(eth_prices_excl_btc[:10])
+# Коэффициенты
+# print("Коэффициент линейной зависимости от BTCUSDT:", results.params[1])
+# print("Константа:", results.params[0])
+
+# Функция для выявления процента изменения
+def has_1pct_change(time_series):
+    min_val = time_series[0]
+    max_val = time_series[0]
 
 
+    for val in time_series:
+        if val < min_val:
+            min_val = val
+        if val > max_val:
+            max_val = val
+
+    pct_change = abs(max_val - min_val) / min_val * 100
+
+    if pct_change >= 1.:
+        return pct_change
+
+    return False
+
+# Вечный цикл оповещений
 while True:
-    latest_data = get_klines('ETHUSDT', Client.KLINE_INTERVAL_1MINUTE, 1)
+    latest_data_eth = get_klines('ETHUSDT', Client.KLINE_INTERVAL_1MINUTE, 60)
+    latest_data_btc = get_klines('BTCUSDT', Client.KLINE_INTERVAL_1MINUTE, 60)
 
+    # Собственные колебания ETHUSDT
+    eth_without_btc = latest_data_eth['Close'].values - results.params[1] * latest_data_btc['Close'].values
+    
+    change = has_1pct_change(eth_without_btc)
+    if change:
+        print(f'В течении последнего часа произошло изменение на {change:.2f}%')
 
-# In this code, we first load the data from a CSV file (ETHUSDT_futures.csv) using pandas. We then separate the data into two sets, one for ETHUSDT and one for BTCUSDT.
-# Next, we standardize the data using z-scores, which will help ensure that both sets of data are on the same scale.
-# We then perform PCA on the BTCUSDT data, using n_components=1 to extract a single component that explains the most variance in the data. We then use the transform method to project the BTCUSDT data onto this component.
-# Finally, we subtract the BTCUSDT component from the ETHUSDT data using the dot function, which effectively removes the movements in ETHUSDT that are caused by movements in BTCUSDT.
-
-# To define ETHUSDT futures price movements excluding BTCUSDT price movements, we can use a statistical method called linear regression analysis. This technique allows us to isolate the effect of BTCUSDT price movements on ETHUSDT futures and identify the unique price movements of ETHUSDT futures.
-# To do this, we would first need to collect historical price data for both ETHUSDT futures and BTCUSDT. We would then use linear regression to model the relationship between the two prices, using BTCUSDT as the independent variable and ETHUSDT as the dependent variable. The resulting equation would allow us to predict the expected value of ETHUSDT futures given the value of BTCUSDT, and the difference between the predicted value and the actual value would represent the unique price movements of ETHUSDT futures.
-# The parameters chosen for the linear regression analysis would depend on the specific dataset being analyzed, including the time period, frequency of data collection, and the number of data points. It is important to choose a large enough dataset to ensure statistical significance, while also balancing this with the need for timely analysis.
-# Once the unique price movements of ETHUSDT futures have been identified, we can monitor the price in real-time and trigger an alert when the price changes by 1% in the last 60 minutes. This can be achieved using a Python program that continuously reads the current price and calculates the percentage change over the past 60 minutes. If the percentage change exceeds 1%, the program can display a message to the console.
-# Overall, the combination of linear regression analysis and real-time monitoring can provide valuable insights into the unique price movements of ETHUSDT futures and help identify potential trading opportunities.
+    time.sleep(60 * 60)
